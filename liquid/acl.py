0f32cd836f74e0a56beca4c6b7e6e1caad792726
Joe Dallago <jd.dallago@gmail.com>
2011-11-07 20:28:55 -0500

    New API changes.

--- liquid/acl.py
+++ liquid/acl.py
@@ -1,5 +1,9 @@
-def add_user(name, parent=None):
-    pass
-
-def add_resource(name, parent=None):
-    pass
+class Acl(object):
+    def __init__(session=None):
+        self.session = session
+        
+    def add_user(name, parent=None):
+        pass
+    
+    def add_resource(name, parent=None):
+        pass
